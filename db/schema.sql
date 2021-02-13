579562d8840c3fd972b30a7e69b6f2870561c34f
C.T. Bell <charlestbell@gmail.com>
2021-02-12 21:18:40 -0500

    Adding more MVC Organization

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,14 @@
 ### Schema
-CREATE DATABASE pets_db;
-USE pets_db;
 
-CREATE TABLE buyers
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	buyer_name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+	burger_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	burger_name varchar(50) NOT NULL,
+    when_devoured DATETIME
 );
 
-CREATE TABLE pets
-(
-	id int NOT NULL AUTO_INCREMENT,
-	animal_breed varchar(255) NOT NULL,
-	animal_name varchar(255) NOT NULL,
-	price int NOT NULL,
-	buyer_id int NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (buyer_id) REFERENCES buyers(id)
-);
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,6 @@
-INSERT INTO buyers (buyer_name) VALUES ('Kevon');
-INSERT INTO buyers (buyer_name) VALUES ('Jane');
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('Whataburger'),('Double Double With Grilled Onions'),('Quarter Pounder With Cheese');
 
-INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('wolf', 'Cassie', 195, 1);
-INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('bear', 'Rachel', 745, 1);
-INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('hawk', 'Tobias', 850, 2);
-INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('gorilla', 'Marco', 400, 2);
-INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('tiger', 'Jake', 300, 2);
+
+SELECT *
+FROM burgers;
